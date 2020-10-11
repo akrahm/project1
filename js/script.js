3717866ef0da6a3824f4d436250393538642e602
Vishal <vishal.kann.14@gmail.com>
2020-10-11 23:07:20 +0530

    my project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+var submit = document.getElementById(submit);
+
+function myFunction1() {
+    window.alert(responses());
+}
+
+function responses(){
+   return "responses are successfully submitted";
+}
